5d957f963961fdd8caceae51bc44e229528c351d
root <micahaile88@gmail.com>
2022-06-06 05:05:08 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print lowere case alphabet in reverse order
+ * Return: 0 success
+ */
+int main(void)
+{
+	char alpha;
+
+	alpha = 'z';
+	while (alpha >= 'a')
+	{
+
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+	return (0);
+}
