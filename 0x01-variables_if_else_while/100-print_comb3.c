042fc8ca2d3ea0dec9da60f3e9aefd1dc76ab00b
root <micahaile88@gmail.com>
2022-06-06 06:41:10 -0700

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print all posible two digit of base 10 nubers
+ * Return: 0 success
+ */
+int main(void)
+{
+	int numb1;
+	int numb2;
+
+	for (numb1 = 0; numb1 <= 8; numb1++)
+	{
+		for (numb2 = 1; numb2 <= 9; numb2++)
+		{
+			putchar((numb1 % 10) + '0');
+			putchar((numb2 % 10) + '0');
+			if (numb1 == 8 && numb2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
