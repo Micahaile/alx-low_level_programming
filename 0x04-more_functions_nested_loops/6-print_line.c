3141243bca2b36507d727671b4f3036907680c98
root <micahaile88@gmail.com>
2022-06-09 14:49:06 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - print straight line
+ * @n: length of line
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int r;
+
+	for (r = 0; r <= n; r++)
+	{
+		if (n == 0 || n < 0)
+			continue;
+		_putchar('_');
+	}
+	_putchar('\n');
+}
