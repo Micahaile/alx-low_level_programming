39d765dea313aaac48947b66973de0632a5d0935
root <micahaile88@gmail.com>
2022-06-09 14:06:03 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - print number 0 to 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		_putchar((n % 10) + '0');
+	_putchar('\n');
+}
