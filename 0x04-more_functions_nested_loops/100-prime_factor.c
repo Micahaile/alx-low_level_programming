dc1d42598ccc35d05b98fd6a5cea83d731e5c6da
root <micahaile88@gmail.com>
2022-06-10 07:31:51 -0700

    task 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+/**
+ * main - print prime factore of 1231952
+ * Return: 0 success
+ */
+int main(void)
+{
+	long pr;
+	long fac;
+
+	pr = 1231952;
+	while (fac < (pr / 2))
+	{
+		if ((pr % 2) == 0)
+		{
+			pr /= 2;
+			continue;
+		}
+		for (fac = 3; fac < (pr / 2); fac += 2)
+		{
+			if ((pr % fac) == 0)
+				pr /= fac;
+		}
+	}
+	printf("%ld\n", pr);
+	return (0);
+}
