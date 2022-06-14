8fe278a602633eda1a7030deb4e440ac65d85f7e
root <micahaile88@gmail.com>
2022-06-14 07:36:38 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - update value of pointer
+ * @n: pointer to take value
+ * Return: 0 sucess
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main- main file
+ */
+void reset_to_98(int *n);
+#endif
