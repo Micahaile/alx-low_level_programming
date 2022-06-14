7c4427479c6aa379c523c3aaee8eabdf02276053
root <micahaile88@gmail.com>
2022-06-14 11:44:01 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,11 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0,  l = 0;
 
-	while (*(str + i))
-	{
-		_putchar(*(str + i));
-		i = i + 2;
-	}
+	while (str[i++])
+		l++;
+	for (i = 0; i < l; i += 2)
+		_putchar(str[i]);
 	_putchar('\n');
 }
