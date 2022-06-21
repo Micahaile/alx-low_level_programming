613247c222dfc72eb29c9b3b0ef8bc0dcf073209
root <micahaile88@gmail.com>
2022-06-21 11:43:03 -0700

    task 6

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+#include <unistd.h>
+
+/**
+ *
+ *  * main - print _putchar
+ *
+ *   * Return: 0 sucess
+ *
+ *    */
+
+int _putchar(char c)
+
+{
+
+		return (write(1, &c, 1));
+
+}
