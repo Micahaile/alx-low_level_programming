7166d8a7bf6b9b9b8d6f50e83ef020d7d5f85801
root <micahaile88@gmail.com>
2022-06-28 11:40:34 -0700

    task 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main -  prints the minimum number of coins to make change for an amount of money
+ * main -  print minimum number of coins to make change for an amount of money
  * @argc: number of argument passed to the program
  * @argv: array of pointer to the program
  * Return: 0 success
